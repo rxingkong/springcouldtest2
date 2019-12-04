a49de591066a19889e07fee6aba2d336ed91043d
17633 <1763331801@qq.com>
2019-12-05 00:49:58 +0800

    springcloudemo

---
+++ userservice/src/main/java/cn/itcast/user/pojo/TbUser.java
@@ -0,0 +1,51 @@
+package cn.itcast.user.pojo;
+
+import java.util.Date;
+
+public class TbUser {
+    private Integer id;
+    private String username;
+    private String password;
+    private String phone;
+    private Date created;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
