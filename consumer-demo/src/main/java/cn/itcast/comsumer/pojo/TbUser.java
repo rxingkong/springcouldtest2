2ed996f521aa0442a0411222abdd7ee5031ef1de
17633 <1763331801@qq.com>
2019-12-10 23:27:34 +0800

    springcloudemo

---
+++ consumer-demo/src/main/java/cn/itcast/comsumer/pojo/TbUser.java
@@ -0,0 +1,59 @@
+package cn.itcast.comsumer.pojo;
+
+import java.util.Date;
+
+
+/*//@Data*/
+public class TbUser {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String phone;
+    private Date created;
+
+    public TbUser() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+}
