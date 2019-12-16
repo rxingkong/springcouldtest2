7e12b5e76be6279d5e4254f7a629ca91fa2e7cd9
admin <1763331801@qq.com>
2019-12-16 16:53:10 +0800

    动态获取列表

--- consumer-demo/src/main/java/cn/itcast/comsumer/controller/ConsumerController.java
+++ consumer-demo/src/main/java/cn/itcast/comsumer/controller/ConsumerController.java
@@ -2,21 +2,29 @@ package cn.itcast.comsumer.controller;
 
 import cn.itcast.comsumer.pojo.TbUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("consumer")
 public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/{id}")
     public TbUser queryById(@PathVariable("id") Long id) {
-        String url = "http://localhost:8081/user/" + id;
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        ServiceInstance serviceInstance = instances.get(0);
+        String url = "http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/user/" + id;
         TbUser tbUser = restTemplate.getForObject(url, TbUser.class);
         return tbUser;
     }
